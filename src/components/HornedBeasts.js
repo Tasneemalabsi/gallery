c164da5c6afe0579f112b0cf6dae94c8d96b62cb
Tasneemalabsi <tasneem.alabsi@gmail.com>
2021-08-09 14:44:17 +0300

    edit

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -7,9 +7,9 @@ class HornedBeasts extends React.Component {
     render () {
         console.log(this.props);
         return (
-            <div class='container'>
+            <div className='container'>
                 <h2>{this.props.title} </h2>
-                <img class='imagedesign' src={this.props.imageUrl} alt='' title='' />
+                <img className='imagedesign' src={this.props.imageUrl} alt='' title='' />
                 <p>{this.props.description}</p>
                 </div>
         );
